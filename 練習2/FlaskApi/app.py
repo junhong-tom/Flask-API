02a2be7816542640fae6eed99f79282406587fe9
junhong-tom <junhong.tom@gmail.com>
2021-06-27 13:46:26 +0800

    在 templates 使用 流程控管及 for loop 流程

--- 練習2/FlaskApi/app.py
+++ 練習2/FlaskApi/app.py
@@ -22,8 +22,13 @@ def hmtlindex():
 @app.route('/render_template')
 def index_template():
     # 傳參數給 template
-    title = 'Flask Web App'
-    return render_template('index.html',title=title)
+    #title = 'Flask Web App'
+    paragraphs =[
+        'section1',
+        'section2',
+        'section3'
+    ]
+    return render_template('index.html',title='Home',data=paragraphs)
 
 if __name__ == '__main__':
     app.run(debug=True) #  debug=True, 可以立即顯示編輯後的內容
