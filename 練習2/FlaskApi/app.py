26927c8dd9b978d52dfaa9e950b87abf6c47a341
junhong-tom <junhong.tom@gmail.com>
2021-06-27 13:30:04 +0800

    使用 render_template  並傳遞參數值

--- 練習2/FlaskApi/app.py
+++ 練習2/FlaskApi/app.py
@@ -1,10 +1,29 @@
 from flask import Flask
 
+# use render_template
+# Check template file
+from flask import render_template
+import os
 app = Flask(__name__)
+
+
 
 @app.route('/')
 def index():
     return "hello,flask"
+
+
+# return html 語言
+@app.route('/html')
+def hmtlindex():
+    return "<h1>hello,flask</h1>"
+
+# 抽取 HTML
+@app.route('/render_template')
+def index_template():
+    # 傳參數給 template
+    title = 'Flask Web App'
+    return render_template('index.html',title=title)
 
 if __name__ == '__main__':
     app.run(debug=True) #  debug=True, 可以立即顯示編輯後的內容
