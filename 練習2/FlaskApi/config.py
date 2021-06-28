602fd8d251ee40da8fec0f81ea4ff3eb4501cb5e
Tom <junhong.tom@gmail.com>
2021-06-28 20:42:41 +0800

    解  a file import b  and b file import a 的問題

--- 練習2/FlaskApi/config.py
+++ 練習2/FlaskApi/config.py
@@ -7,6 +7,12 @@ class Config(object):
     # SECRET KEY
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'A-VERY-LONG-SECRET-KEY'
 
+    # Google reCAPTCHA 驗證 可以使用 FAQ 文件 所提供的 test key 跳過申請步驟
+    # https://medium.com/pyladies-taiwan/flask-wtf-%E8%A1%A8%E5%96%AE%E9%A9%97%E8%AD%89-4b4423eeeb45
+    RECAPTCHA_PUBLIC_KEY = '6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI'
+    RECAPTCHA_PRIVATE_KEY = '6LeIxAcTAAAAAGG-vFI1TnRWxMZNFuojJ4WifJWe'
+
+
     # 先從環境變量獲取 Database 的 URL ，如果沒有則從本地創建一個 app.db
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' +os.path.join(basedir,'app.db')
 
--- 練習2/FlaskApi/forms.py
+++ 練習2/FlaskApi/forms.py
@@ -1,16 +1,24 @@
 from flask_wtf import FlaskForm
+from flask_wtf.recaptcha import RecaptchaField
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import  DataRequired, Length, Email,EqualTo
+
+
 #  如果遇到這樣的狀況: Exception: Install 'email_validator' for email validation support.
 #  解決方法: 需要 Install 套件  email_validator
 
 #  如果遇到這樣的狀況: RuntimeError: A secret key is required to use CSRF.
 #  解決方法: configure 設定
 
+#  如果遇到這樣的狀況: RuntimeError: RECAPTCHA_PUBLIC_KEY config not set
+#  解決方法: configure 設定
+
 class RegisterForm(FlaskForm):
-    username = StringField('Username',validators=[DataRequired(),Length(min=6,max=20)])
+    username = StringField('Username',validators=[DataRequired(),Length(min=3,max=20)])
     email = StringField('Email', validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired(), Length(min=8, max=20)])
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=3, max=20)])
     confirm = PasswordField('Repeat Password', validators=[DataRequired(), EqualTo('password')])
+    recaptcha = RecaptchaField()
+
     submit = SubmitField('Register')
     pass
